5b456f3422769fb72da2ee4114a5e3f5fe860dab
Alexsandrab <sandrinhaleto@hotmail.com>
2021-11-09 19:04:29 -0300

    primeiro commit

---
+++ logica.js
@@ -0,0 +1,13 @@
+const input = require("readline-sync");
+
+let maca = "Maça";
+let pera = "Perâ";
+let cachorro = "Princesa";
+let gato = "Miau";
+let cachorro2 = "Nega mãe"
+
+console.log(maca);
+console.log(pera);
+console.log(cachorro);
+console.log(gato);
+console.log(cachorro2);
